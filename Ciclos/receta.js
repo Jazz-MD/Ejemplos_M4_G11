d652b7378f819215c15f2238116683a3e0000d18
Mauricio Fuentes <mauriciofb@gmail.com>
2024-07-19 14:02:06 -0400

    Nuevos ejemplos

---
+++ Ciclos/receta.js
@@ -0,0 +1,9 @@
+function cocinarArroz(porciones) {
+  document.write(`
+    <p>${porciones} tazas de arroz</p>
+    <p>${porciones * 2} tazas de agua</p>
+    <p>${porciones * 0.02} gramos de sal</p>
+    <p>${porciones} dientes de ajo</p>
+    <p>Cocinar a fuego lento, revoltosa 🩰💃🏼</p>
+    `)
+}
