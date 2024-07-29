95a084851f85aa66364f765b699f12bddbe90b63
Mauricio Fuentes <mauriciofb@gmail.com>
2024-07-29 11:19:56 -0400

    Nuevo ejemplo jQuery

---
+++ jQuery/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  // let text = document.getElementById("text");
+  // text.addEventListener('click', () => {
+  //   document.write('click sobre el texto')
+  // })
+
+  let text = $('.text')
+  let entrada = $('.entrada')
+  let btn = $('.btn')
+  let divGenerico = $("div.generica")
+
+  console.dir(divGenerico)
+  console.dir(text)
+  console.dir(entrada)
+  console.dir(btn)
+
+})
